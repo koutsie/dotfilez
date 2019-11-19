b2bbe3c6212401fece23782c3fcb06a8da3ba5fc
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-19 22:15:58 +0200

    Updated config.def.h
    Don't forget to check your bindings!

--- sowm/config.def.h
+++ sowm/config.def.h
@@ -1,16 +1,19 @@
+// Edited version of "config.def.h" for sowm
+
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #define MOD Mod4Mask
 
-const char* menu[]    = {"dmenu_run",      0};
-const char* term[]    = {"xfce4-terminal",             0};
-const char* scrot[]   = {"/home/koutsie/Scripts/screenshot.sh",            0};
-const char* briup[]   = {"bri", "10", "+", 0};
-const char* bridown[] = {"bri", "10", "-", 0};
-const char* voldown[] = {"amixer", "sset", "Master", "5%-",         0};
-const char* volup[]   = {"amixer", "sset", "Master", "5%+",         0};
-const char* volmute[] = {"amixer", "sset", "Master", "toggle",      0};
+const char* menu[]    = {"dmenu_run", 0};
+const char* music[]   = {"/home/koutsie/Scripts/mpsyt.sh", 0};
+const char* term[]    = {"xfce4-terminal", 0};
+const char* scrot[]   = {"/home/koutsie/Scripts/screenshot.sh", 0};
+const char* briup[]   = {"/home/koutsie/Scripts/b_up.sh", 0};
+const char* bridown[] = {"/home/koutsie/Scripts/b_down.sh" , 0};
+const char* voldown[] = {"/home_koutsie/Scripts/vol_down.sh", 0};
+const char* volup[]   = {"/home/koutsie/Scripts/vol_up.sh", 0};
 const char* colors[]  = {"/home/koutsie/Scripts/wallpaper.sh", 0};
 
 static struct key keys[] = {
@@ -19,15 +22,24 @@ static struct key keys[] = {
     {MOD,      XK_f,   win_fs,     {0}},
     {Mod1Mask, XK_Tab, win_next,   {0}},
 
+// Added: mpsyt hotkey
+    {MOD, XK_m,      run, {.com = music}},
+
+
+// Added: dmenu#2 hotkey
+    {MOD, XK_Control_R,      run, {.com = menu}},
+
     {MOD, XK_d,      run, {.com = menu}},
     {MOD, XK_w,      run, {.com = colors}},
     {MOD, XK_p,      run, {.com = scrot}},
     {MOD, XK_Return, run, {.com = term}},
 
+// Added: so my F2(AudioLowerVolume) is doing odd stuff so i made the F1(XF86AudioMute) my AudioLowerVolume key
+    {0,   XF86XK_AudioMute,         run, {.com = voldown}},
     {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
     {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
-    {0,   XF86XK_AudioMute,         run, {.com = volmute}},
-    {0,   XF86XK_MonBrightnessUp,   run, {.com = briup}},
+
+    {0,   XF86XK_MonBrightnessUp,  run, {.com = briup}},
     {0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},
 
     {MOD,           XK_1, ws_go,     {.i = 1}},

2a51367d1390cae1ebd310ff13c4e201e75312bd
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-20 22:48:19 +0200

    Added bindings for changing local wallpaper
    Its x.
    
    Yes, the key X.

--- sowm/config.def.h
+++ sowm/config.def.h
@@ -1,20 +1,20 @@
-// Edited version of "config.def.h" for sowm
-
-
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #define MOD Mod4Mask
 
-const char* menu[]    = {"dmenu_run", 0};
+const char* menu[]    = {"/home/koutsie/Scripts/dmenu.sh", 0};
 const char* music[]   = {"/home/koutsie/Scripts/mpsyt.sh", 0};
 const char* term[]    = {"xfce4-terminal", 0};
-const char* scrot[]   = {"/home/koutsie/Scripts/screenshot.sh", 0};
+const char* screenshot[]   = {"/home/koutsie/Scripts/screenshot.sh", 0};
 const char* briup[]   = {"/home/koutsie/Scripts/b_up.sh", 0};
 const char* bridown[] = {"/home/koutsie/Scripts/b_down.sh" , 0};
-const char* voldown[] = {"/home_koutsie/Scripts/vol_down.sh", 0};
-const char* volup[]   = {"/home/koutsie/Scripts/vol_up.sh", 0};
+const char* vold[] = {"/home_koutsie/Scripts/vol_down.sh", 0};
+const char* volp[]   = {"/home/koutsie/Scripts/vol_up.sh", 0};
 const char* colors[]  = {"/home/koutsie/Scripts/wallpaper.sh", 0};
+const char* favw[]    = {"/home/koutsie/Scripts/fav_wall.sh", 0};
+const char* restorew[]    = {"/home/koutsie/Scripts/getwall.sh", 0};
+
 
 static struct key keys[] = {
     {MOD,      XK_q,   win_kill,   {0}},
@@ -27,17 +27,26 @@ static struct key keys[] = {
 
 
 // Added: dmenu#2 hotkey
-    {MOD, XK_Control_R,      run, {.com = menu}},
+
+    {0, XK_Control_R,      run, {.com = menu}},
 
     {MOD, XK_d,      run, {.com = menu}},
     {MOD, XK_w,      run, {.com = colors}},
-    {MOD, XK_p,      run, {.com = scrot}},
+    {MOD, XK_p,      run, {.com = screenshot}},
     {MOD, XK_Return, run, {.com = term}},
 
-// Added: so my F2(AudioLowerVolume) is doing odd stuff so i made the F1(XF86AudioMute) my AudioLowerVolume key
-    {0,   XF86XK_AudioMute,         run, {.com = voldown}},
-    {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
-    {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
+
+// Added: favourite wallpaper key | Added: get local wallpaper
+
+    {MOD, XK_z, run, {.com = favw}},
+    {MOD, XK_x, run, {.com = restorew}},
+
+
+// Added: everything is broken about volume in my system i guess.
+
+    {0,   XF86XK_AudioLowerVolume,  run, {.com = vold}},
+    {0,   XF86XK_AudioRaiseVolume,  run, {.com = volp}},
+
 
     {0,   XF86XK_MonBrightnessUp,  run, {.com = briup}},
     {0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},

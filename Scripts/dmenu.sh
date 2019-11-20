54110fbfc9afbf5a1dd0cdd147653a9738a52f55
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-20 22:15:23 +0200

    Removed some useless stuff.

--- Scripts/dmenu.sh
+++ Scripts/dmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec dmenu_path | dmenu_run -nb '#ff0000' -nf '#ffffff' -sb '#990000' -fn 'terminus-14' -b # && eval "exec $exe"
+exec dmenu_path | dmenu_run -nb '#ff0000' -nf '#ffffff' -sb '#990000' -fn 'terminus-14' -b

919bb442e890e2d238803c92738fc79146119aec
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-21 00:06:32 +0200

    Now this is compatible with everyone!
    Yay!

--- installer/installer.sh
+++ installer/installer.sh
@@ -13,28 +13,21 @@ echo installing to /home/"$USER"/
 echo getting dotfilez
 git clone https://github.com/koutsie/dotfilez.git
 
-echo entering directory dotfilez
-cd dotfilez
+echo "Baking a special cake, just for $USER"
+sed -i "s/koutsie/$USER/g" sowm/config.def.h
+sed -i "s/koutsie/$USER/g" sowm.desktop
 
 echo making directories
 mkdir /home/"$USER"/Wallpapers
 echo New dir: Wallpapers
 mkdir /home/"$USER"/Scripts
 echo New dir: Scripts
-mkdir /home/"$USER"/.config/i3
-echo New dir: .config/i3
-mkdir /home/"$USER"/.config/neofetch
-echo New dir: .config/neofetch
-
-echo copying configs
-cp config/i3/config /home/"$USER"/.config/i3
-cp config/neofetch/config.conf /home/"$USER"/.config/neofetch
 
 echo copying scripts
 cp --verbose Scripts/* /home/"$USER"/Scripts
 
 
-echo adding bashrc
+echo adding bashrc's
 cp --verbose .bashrc /home/"$USER"/.bashrc
 
 cp --verbose .bashrc_aliases /home/"$USER"/.bashrc_aliases
@@ -48,8 +41,7 @@ cp --verbose .bashrc_user /home/"$USER"/.bashrc_user
 echo getting sowm
 git clone https://github.com/dylanaraps/sowm sowm_source 
 cp --verbose sowm/config.def.h sowm_source/config.def.h
-echo entered direcotry sowm_source && cd sowm_source && make && sudo make clean install && cd .. && echo backing up from sowm_source
+cd sowm_source && make && sudo make install && cd ..
 
 sudo cp --verbose sowm.desktop /usr/share/xsessions/sowm.desktop
-
 

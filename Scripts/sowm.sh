9eabdb3fe96ea9e2b5a0277453a1fbd0a776a54e
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-21 21:04:44 +0200

    Testing error handling.

--- Scripts/sowm.sh
+++ Scripts/sowm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-screen -dmS sowm /usr/bin/sowm &
+screen -dmS sowm /usr/bin/sowm
 dunstify "Welcome to sowm, $USER" 
 
-/home/"$USER"/Scripts/getwall.sh
-polybar minimax -q
+/home/"$USER"/Scripts/getwall.sh &
+polybar minimax -q 2>&1 1>/dev/null | while read OUTPUT; do dunstify -u critical "sowm" "$OUTPUT"; done &
 sleep infinity

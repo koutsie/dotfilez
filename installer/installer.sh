34fef8ba3c3caf225c93238e105a14bd472e7f82
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-21 19:54:02 +0200

    Added colors so stuff is easier to identify.

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 red=`tput setaf 116`
 ylw=`tput setaf 213`
-ond=`tput setaf 212`
+pink=`tput setaf 212`
+rnd=`tput setaf 208`
 bold=`tput bold`
 rs=`tput sgr0`
 
@@ -47,51 +48,51 @@ donate=$(cat << EOF
         
         
         
-              ${ond}${pink}0x02a17dbf3d15962150e4efe8f74e2491d733629f${rs}
+              ${pink}0x02a17dbf3d15962150e4efe8f74e2491d733629f${rs}
                                                                           
 EOF
 )
 
-echo installing to /home/"$USER"/
+echo "${rnd}Installing to ${pink}/home/"$USER"/${rs}"
 
 # --------------------------------------
 printf "\n\n\n\n"
 
-echo getting dotfilez
+echo "${rnd}Getting dotfilez${rs}"
 git clone https://github.com/koutsie/dotfilez.git
 cd dotfilez
 
 # --------------------------------------
 printf "\n\n\n\n"
 
-echo "Baking a special cake, just for $USER"
+echo "${rnd}Baking a special cake, just for ${pink}$USER${rs}"
 sed -i "s/koutsie/$USER/g" sowm/config.def.h
 sed -i "s/koutsie/$USER/g" sowm.desktop
 
 # --------------------------------------
 printf "\n\n\n\n"
 
-echo "making directories"
+echo "${rnd}Making directories${rs}"
 mkdir /home/"$USER"/Wallpapers
-echo "New dir: Wallpapers"
+echo "${rnd}New dir: ${ylw}Wallpapers${rs}"
 
 mkdir /home/"$USER"/Scripts
-echo "New dir: Scripts"
+echo "${rnd}New dir: ${ylw}Scripts${rs}"
 
 mkdir /home/"$USER"/Scripts/poly
-echo "New dir: Scripts/poly"
+echo "${rnd}New dir: ${ylw}Scripts/poly${rs}"
 
 mkdir /home/"$USER"/.config/polybar
-echo "New dir: .config/polybar"
+echo "${rnd}New dir: ${ylw}.config/polybar${rs}"
 
 mkdir /home/"$USER"/dunst
-echo "New dir .config/dunst"
+echo "${rnd}New dir ${ylw}.config/dunst${rs}"
 
 
 # --------------------------------------
 printf "\n\n\n\n"
 
-echo "copying configs and scripts"
+echo "${rnd}Copying configs and scripts${rs}"
 cp -r --verbose config/polybar/* /home/"$USER"/.config/
 cp -r --verbose config/dunst/* /home/"$USER"/.config/
 cp -r --verbose config/neofetch/* /home/"$USER"/.config/
@@ -103,7 +104,7 @@ cp -r --verbose Scripts/poly/ /home/"$USER"/Scripts/
 chmod +x /home/"$USER"/Scripts/*
 chmod +x /home/"$USER"/Scripts/poly/*
 
-echo "adding bashrcs"
+echo "${rnd}Adding bashrcs${rs}"
 cp --verbose .bashrc /home/"$USER"/.bashrc
 
 cp --verbose .bashrc_aliases /home/"$USER"/.bashrc_aliases
@@ -114,7 +115,7 @@ cp --verbose .bashrc_user /home/"$USER"/.bashrc_user
 printf "\n\n\n\n"
 
 # sowm stuff
-echo "getting sowm"
+echo "${rnd}getting, compiling and installing ${pink}sowm${rs}"
 git clone https://github.com/dylanaraps/sowm sowm_source 
 cp --verbose sowm/config.def.h sowm_source/config.def.h
 cd sowm_source && make -B && sudo make install && cd ..

f747d48624f56e3cad831486e881c2be6b485220
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-21 19:23:40 +0200

    reworked the whole thing.

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,62 +1,21 @@
 #!/bin/bash
-echo "██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗";
-echo "██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝╚══███╔╝";
-echo "██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗    ███╔╝ ";
-echo "██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝   ███╔╝  ";
-echo "██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████╗";
-echo "╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝";
-echo "                                                             ";
+red=`tput setaf 116`
+ylw=`tput setaf 213`
+ond=`tput setaf 212`
+bold=`tput bold`
+rs=`tput sgr0`
 
-
-echo installing to /home/"$USER"/
-
-echo getting dotfilez
-git clone https://github.com/koutsie/dotfilez.git
-cd dotfilez
-
-echo "Baking a special cake, just for $USER"
-sed -i "s/koutsie/$USER/g" sowm/config.def.h
-sed -i "s/koutsie/$USER/g" sowm.desktop
-
-echo making directories
-mkdir /home/"$USER"/Wallpapers
-echo New dir: Wallpapers
-mkdir /home/"$USER"/Scripts
-echo New dir: Scripts
-mkdir /home/"$USER"/Scripts/poly
-echo New dir: poly
-
-echo copying scripts
-cp --verbose Scripts/* /home/"$USER"/Scripts
-cp --verbose Scripts/poly/* /home/"$USER"/Scripts/poly
-chmod +x /home/"$USER"/Scripts/*
-chmod +x /home/"$USER"/Scripts/poly/*
-
-echo adding bashrcs
-cp --verbose .bashrc /home/"$USER"/.bashrc
-
-cp --verbose .bashrc_aliases /home/"$USER"/.bashrc_aliases
-
-cp --verbose .bashrc_user /home/"$USER"/.bashrc_user
-
-
-
-
-# sowm stuff
-echo getting sowm
-git clone https://github.com/dylanaraps/sowm sowm_source 
-cp --verbose sowm/config.def.h sowm_source/config.def.h
-cd sowm_source && make -B && sudo make install && cd ..
-
-sudo cp --verbose sowm.desktop /usr/share/xsessions/sowm.desktop
-
-echo "Done, enjoy!"
-cat << EOF
+donate=$(cat << EOF
+       ${ylw}██████╗  ██████╗ ████████╗${red}███████╗██╗██╗     ███████╗███████╗${rs}
+       ${ylw}██╔══██╗██╔═══██╗╚══██╔══╝${red}██╔════╝██║██║     ██╔════╝╚══███╔╝${rs}
+       ${ylw}██║  ██║██║   ██║   ██║   ${red}█████╗  ██║██║     █████╗    ███╔╝ ${rs}
+       ${ylw}██║  ██║██║   ██║   ██║   ${red}██╔══╝  ██║██║     ██╔══╝   ███╔╝  ${rs}
+       ${ylw}██████╔╝╚██████╔╝   ██║   ${red}██║     ██║███████╗███████╗███████╗${rs}
+       ${ylw}╚═════╝  ╚═════╝    ╚═╝   ${red}╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝${rs}
                                   Donate?
                                                                           
-                                                                          
-                                                                          
-        ##############  ##          ######      ##  ##############        
+                                                                                                    
+        ${bold}##############  ##          ######      ##  ##############        
         ##          ##  ##    ######  ##  ######    ##          ##        
         ##  ######  ##  ####    ####      ######    ##  ######  ##        
         ##  ######  ##  ######  ########      ##    ##  ######  ##        
@@ -84,11 +43,83 @@ cat << EOF
         ##  ######  ##    ##      ######  ####  ##  ##      ##            
         ##  ######  ##      ##            ##    ##  ##        ####        
         ##          ##  ####    ##  ######  ####  ##  ######  ####        
-        ##############  ####  ##  ####  ##  ##  ######  ####  ##          
+        ##############  ####  ##  ####  ##  ##  ######  ####  ##${rs}          
         
         
         
-              0x02a17dbf3d15962150e4efe8f74e2491d733629f
+              ${ond}${pink}0x02a17dbf3d15962150e4efe8f74e2491d733629f${rs}
                                                                           
                                                                           
-EOF
+EOF)
+
+echo installing to /home/"$USER"/
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+echo getting dotfilez
+git clone https://github.com/koutsie/dotfilez.git
+cd dotfilez
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+echo "Baking a special cake, just for $USER"
+sed -i "s/koutsie/$USER/g" sowm/config.def.h
+sed -i "s/koutsie/$USER/g" sowm.desktop
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+echo "making directories"
+mkdir /home/"$USER"/Wallpapers
+echo "New dir: Wallpapers"
+
+mkdir /home/"$USER"/Scripts
+echo "New dir: Scripts"
+
+mkdir /home/"$USER"/Scripts/poly
+echo "New dir: Scripts/poly"
+
+mkdir /home/"$USER"/.config/polybar
+echo "New dir: .config/polybar"
+
+mkdir /home/"$USER"/dunst
+echo "New dir .config/dunst"
+
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+echo "copying configs and scripts"
+cp --verbose config/poly* /home/"$USER"/.config/polybar
+cp --verbose config/dunst* /home/"$USER"/.config/dunst
+
+
+cp --verbose Scripts/* /home/"$USER"/Scripts
+cp --verbose Scripts/poly* /home/"$USER"/Scripts/poly
+chmod +x /home/"$USER"/Scripts/*
+chmod +x /home/"$USER"/Scripts/poly/*
+
+echo "adding bashrcs"
+cp --verbose .bashrc /home/"$USER"/.bashrc
+
+cp --verbose .bashrc_aliases /home/"$USER"/.bashrc_aliases
+
+cp --verbose .bashrc_user /home/"$USER"/.bashrc_user
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+# sowm stuff
+echo "getting sowm"
+git clone https://github.com/dylanaraps/sowm sowm_source 
+cp --verbose sowm/config.def.h sowm_source/config.def.h
+cd sowm_source && make -B && sudo make install && cd ..
+
+sudo cp --verbose sowm.desktop /usr/share/xsessions/sowm.desktop
+
+# --------------------------------------
+printf "\n\n\n\n"
+
+echo "$donate"

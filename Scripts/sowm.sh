f9f31525c65147867005ac7bbaf60d403ea7ed7c
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-21 03:48:16 +0200

    Prepparing for polybar...

--- Scripts/sowm.sh
+++ Scripts/sowm.sh
@@ -2,8 +2,6 @@
 screen -dmS sowm /usr/bin/sowm &
 dunstify "Welcome to sowm, $USER" 
 
-sleep 10 && /home/"$USER"/Scripts/updates.sh 
 /home/"$USER"/Scripts/getwall.sh
-screen -dmS battmon /home/"$USER"/Scripts/battery.sh 
-
+polybar minimax -q
 sleep infinity

830fe0d01a801aac768fe3df4a35338c2ddd36ce
koutsie <18449778+koutsie@users.noreply.github.com>
2019-11-22 03:48:54 +0200

    updated.

--- Scripts/vol_down.sh
+++ Scripts/vol_down.sh
@@ -3,5 +3,5 @@
 current=$(pamixer --get-volume)
 volume=$(pamixer --get-volume-human)
 
-pamixer -d 10
+pamixer -d 5
 dunstify $volume
--- Scripts/vol_up.sh
+++ Scripts/vol_up.sh
@@ -3,5 +3,5 @@
 current=$(pamixer --get-volume)
 volume=$(pamixer --get-volume-human)
 
-pamixer -i 10
+pamixer -i 5
 dunstify $volume

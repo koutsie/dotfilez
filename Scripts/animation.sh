8d44bac160ac28480ca15cdf411fb6bd93281b9a
koutsie <18449778+koutsie@users.noreply.github.com>
2020-05-30 20:09:09 +0300

    Fixed grammar.

--- Scripts/animation.sh
+++ Scripts/animation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Tchooses a random boot animation!
+# This chooses a random animation to show when sowm starts. Uses mpv and pamixer.
 cd /home/$USER/sounds/
-pamixer --set-volume 50
+pamixer --set-volume 40 
 animation=$(ls | shuf -n1)
 mpv --fs "$animation"

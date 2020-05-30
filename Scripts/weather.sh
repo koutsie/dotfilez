64ec0f71b961a55775a2dbe6769bbd3b386d661d
koutsie <18449778+koutsie@users.noreply.github.com>
2020-05-30 20:06:55 +0300

    Update weather.sh

--- Scripts/weather.sh
+++ Scripts/weather.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# This script handles getting the weather and caching it for later use so we dont overload the wttr.in server with out requests every time we change a wallpaper.
+
 for (( ; ; ))
 do
    sleep 5
